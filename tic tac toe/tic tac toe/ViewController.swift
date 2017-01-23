acea20191778acbe0ab4c08ebd3fef0eed712c0a
Juan Luis Flores <a01280767@itesm.mx>
2017-01-23 13:25:32 -0600

    tic tac toe update

--- tic tac toe/tic tac toe/ViewController.swift
+++ tic tac toe/tic tac toe/ViewController.swift
@@ -17,69 +17,69 @@ class ViewController: UIViewController {
     var gameIsActive = true
     
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var labelTURN: UILabel!
+    @IBOutlet weak var playAgainButton: UIButton!
+
     
-    @IBAction func action(_ sender: AnyObject)
-    {
-        if (gameState[sender.tag-1] == 0 && gameIsActive == true)
-        {
+    @IBAction func action(_ sender: AnyObject) {
+        if (gameState[sender.tag-1] == 0 && gameIsActive == true) {
+            
             gameState[sender.tag-1] = activePlayer
             
-            if (activePlayer == 1)
-            {
+            if (activePlayer == 1) {
+                
                 sender.setImage(UIImage(named: "Cross.png"), for: UIControlState())
+                labelTURN.text = "PLAYER 2"
                 activePlayer = 2
-            }
-            else
-            {
+            } else {
+                
                 sender.setImage(UIImage(named: "Nought.png"), for: UIControlState())
+                labelTURN.text = "PLAYER 1"
                 activePlayer = 1
+
             }
         }
-    }
-        /*
-        for combination in winningCombinations
-        {
-            if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]
-            {
+        
+        for combination in winningCombinations {
+            if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
+                
                 gameIsActive = false
                 
-                if gameState[combination[0]] == 1
-                {
+                if gameState[combination[0]] == 1 {
+                    
                     label.text = "CROSS HAS WON!"
-                }
-                else
-                {
+                } else {
+                    
                     label.text = "NOUGHT HAS WON!"
                 }
-                
                 playAgainButton.isHidden = false
                 label.isHidden = false
+                
             }
         }
         
         gameIsActive = false
         
-        for i in gameState
-        {
-            if i == 0
-            {
+        for i in gameState {
+            
+            if (i == 0) {
+               
                 gameIsActive = true
                 break
             }
         }
         
-        if gameIsActive == false
-        {
+        if gameIsActive == false {
+            
             label.text = "IT WAS A DRAW"
             label.isHidden = false
             playAgainButton.isHidden = false
         }
+    
     }
-    
-    
-    @IBOutlet weak var playAgainButton: UIButton!
-    @IBAction func playAgain(_ sender: AnyObject)
-    {
+
+    @IBAction func playAgain(_ sender: AnyObject) {
+        
         gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
         gameIsActive = true
         activePlayer = 1
@@ -87,13 +87,12 @@ class ViewController: UIViewController {
         playAgainButton.isHidden = true
         label.isHidden = true
         
-        for i in 1...9
-        {
+        for i in 1...9 {
+          
             let button = view.viewWithTag(i) as! UIButton
             button.setImage(nil, for: UIControlState())
         }
-    }*/
-
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
