f43fcf49cadd318a7d0e9caa388369933d1ed595
Juan Luis Flores <a01280767@itesm.mx>
2017-01-23 13:26:04 -0600

    Auto Layout init commit

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by alumno on 1/23/17.
+//  Copyright © 2017 Juan Luis Flores. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var vista1: UIView!
+    @IBOutlet weak var vista2: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
