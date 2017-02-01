60583ad78d6651897596edfeb0391e97c304a239
Juan Luis Flores <jluisfgarza@gmail.com>
2017-02-01 14:06:49 -0600

    tarea 3 initial commit

---
+++ Cuerpos	Geométricos/Cuerpos	Geométricos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cuerpos	Geométricos
+//
+//  Created by Abril González on 01/02/17.
+//  Copyright © 2017 Juan Luis Flores. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
